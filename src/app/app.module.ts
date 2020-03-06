43de323f865a55af63deec51d18a4ec16a3b79d1
Marcelo Poblete <marcelopoblette@gmail.com>
2020-03-06 16:18:31 -0300

    employeeList creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmplyeeListComponent } from './emplyee-list/emplyee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmplyeeListComponent
   ],
   imports: [
     BrowserModule,
